f9dbbaa1c3f4cc7e4e578b7cc8ad468d91d2f967
dwc13 <44411887+dwc13@users.noreply.github.com>
2022-06-06 12:50:00 -0500

    Modify readability of output

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-echo "Installing Applications:\n\tVim\n\tGit"
+echo -e "Installing Applications:\n\tVim\n\tGit"
+for a in {1..35}; do echo -n =; done
+echo -e "\n"  
 apt install -y vim git
 
 
+echo -e "\nGenerating Symbolic Links:"
+for a in {1..35}; do echo -n =; done
+
+echo -e "\n======= Vimrc Link ======="
 ln -s $(pwd)/.vimrc ~/.vimrc
+
+echo -e "======= Bashrc Link ======="
 ln -s $(pwd)/.bashrc ~/.bashrc
+
+echo -e "======= Git Config Link ======="
 ln -s $(pwd)/.gitconfig ~/.gitconfig
