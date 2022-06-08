e99b3c4b970772a81523de9d53c0bc9a4ac80adf
dwc13 <44411887+dwc13@users.noreply.github.com>
2022-06-08 13:51:59 -0500

    Update Script

--- initialize-dotfile.sh
+++ initialize-dotfile.sh
@@ -1,21 +1,57 @@
 #!/bin/bash
 
-echo -e "Installing Applications:\n\tVim\n\tGit\nDocker"
+echo -e "Installing Applications:\n\tVim\n\tDocker"
 for a in {1..35}; do echo -n =; done
-echo -e "\n"  
-sudo apt install -y vim git docker
+echo -e "\n"
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
+sudo apt install -y vim docker
 
+# Install Flatpak
+# apt install -y flatpak
+# flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 echo -e "\nGenerating Symbolic Links:"
 for a in {1..35}; do echo -n =; done
 
+FILE_VIM="/home/dom/.vimrc"
 echo -e "\n======= Vimrc Link ======="
-ln -s ~/.dotfile/.vimrc ~/.vimrc
+if [ -f "$FILE_VIM" ]; then {
+	rm $FILE_VIM
+	ln -s $(pwd)/.vimrc $FILE_VIM
+	echo "Deleted $FILE_VIM and Relinked"
+}
+else {
+	ln -s $(pwd)/.vimrc $FILE_VIM
+	echo "Linked $FILE_VIM"
+}
+fi	
 
+FILE_BASH="/home/dom/.bashrc"
 echo -e "======= Bashrc Link ======="
-ln -s ~/.dotfile/.bashrc ~/.bashrc
+if [ -f "$FILE_BASH" ]; then {
+	rm $FILE_BASH
+	ln -s $(pwd)/.bashrc $FILE_BASH
+	echo "Deleted $FILE_BASH and Relinked"
+}
+else {
+	ln -s $(pwd)/.bashrc $FILE_BASH
+	echo "Linked $FILE_BASH"
+}
+fi	
 
+FILE_GIT="/home/dom/.gitconfig"
 echo -e "======= Git Config Link ======="
-ln -s ~/.dotfile/.gitconfig ~/.gitconfig
+if [ -f "$FILE_GIT" ]; then {
+	rm $FILE_GIT
+	ln -s $(pwd)/.gitconfig $FILE_GIT
+	echo "Deleted $FILE_GIT and Relinked"
+}
+else {
+	ln -s $(pwd)/.gitconfig $FILE_GIT
+	echo "Linked $FILE_GIT"
+}
+fi	
 
-source ~/.bashrc
+source $FILE_BASH
