6cb9f39b451b498b09e0489949c9ac711d543582
dwc13 <44411887+dwc13@users.noreply.github.com>
2025-02-26 23:25:19 -0600

    Alter Symbolic Link for HOME dir

--- init-dot-arch.sh
+++ init-dot-arch.sh
@@ -41,16 +41,17 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 for a in {1..35}; do echo -n =; done
 echo -e "\nGenerating Symbolic Links:"
 
+HOME="/home/dom"
 ##### VIMRC #####
 FILE_VIM="/home/dom/.vimrc"
 echo -e "\n======= Vimrc Link ======="
 if [ -f "$FILE_VIM" ]; then {
 	rm $FILE_VIM
-	ln -s $(pwd)/.dotfile/.vimrc-arch $FILE_VIM
+	ln -s $HOME/.dotfile/.vimrc $FILE_VIM
 	echo "Deleted $FILE_VIM and Relinked"
 }
 else {
-	ln -s $(pwd)/.dotfile/.vimrc-arch $FILE_VIM
+	ln -s $HOME/.dotfile/.vimrc $FILE_VIM
 	echo "Linked $FILE_VIM"
 }
 fi	
@@ -60,11 +61,11 @@ FILE_GIT="/home/dom/.gitconfig"
 echo -e "======= Git Config Link ======="
 if [ -f "$FILE_GIT" ]; then {
 	rm $FILE_GIT
-	ln -s $(pwd)/.gitconfig $FILE_GIT
+	ln -s $HOME/.dotfile/gitconfig $FILE_GIT
 	echo "Deleted $FILE_GIT and Relinked"
 }
 else {
-	ln -s $(pwd)/.gitconfig $FILE_GIT
+	ln -s $HOME/.dotfile/.gitconfig $FILE_GIT
 	echo "Linked $FILE_GIT"
 }
 fi	
@@ -74,11 +75,11 @@ FILE_BASH="/home/dom/.bashrc"
 echo -e "======= Bashrc Link ======="
 if [ -f "$FILE_BASH" ]; then {
 	rm $FILE_BASH
-	ln -s $(pwd)/.dotfile/.bashrc-arch $FILE_BASH
+	ln -s $HOME/.dotfile/.bashrc-arch $FILE_BASH
 	echo "Deleted $FILE_BASH and Relinked"
 }
 else {
-	ln -s $(pwd)/.dotfile/.bashrc-arch $FILE_BASH
+	ln -s $HOME/.dotfile/.bashrc-arch $FILE_BASH
 	echo "Linked $FILE_BASH"
 }
 fi	
