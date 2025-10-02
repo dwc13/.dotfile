f5d716694ba8c38ce60707cb75b55bafbe1be4a7
dwc13 <dominic.wade9@gmail.com>
2025-10-02 00:45:34 -0500

    Fix AUR and ARCH installs

--- init-dot-arch.sh
+++ init-dot-arch.sh
@@ -8,6 +8,7 @@ cd ~
 echo -e "Updating System\n"
 linebar
 sudo pacman -Syu
+sudo pacman -Sy cosmic
 
 echo -e "Installing Programs\n"
 linebar
@@ -15,13 +16,15 @@ sudo pacman -Sy curl tmux git yazi tree \
   ripgrep fzf vim lazygit neovim \
   evince shotwell fastfetch vivaldi \
   bitwarden kwallet kwalletmanager kwallet-pam \
-  ghostty cosmic displaylink rofi opencode \
+  ghostty rofi code \
   networkmanager networkmanager-openconnect openconnect
 sudo pacman -S python3 python-pip
 sudo pacman -S --needed git base-devel yay
 sudo pacman -S linux-headers
 
 git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+
+yay -Sy opencode displaylink code-features
 
 echo -e "\nGenerating Symbolic Links:"
 linebar
